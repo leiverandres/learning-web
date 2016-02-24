4baf7e1bc464b7ed3821555baad9e6ddd2d217f8
Leiver Andres Campeon <leiverandres04p@hotmail.com>
2016-02-23 21:47:56 -0500

    Update app.js

--- node/express_acamica/app.js
+++ node/express_acamica/app.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
-var routes = require('./config/routes');
-
 var app = express();
+
+app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-routes(app);
+function obtenerCurso(req, res, next, id) {
+	setTimeout(function() {
+      res.curso = 'express';
+      next();
+	}, 2000);
+}
 
-app.listen(8080, ()=>{
-  console.log("listening on port 8080");
+var cursos = express.Router();
+
+cursos.param('id', obtenerCurso);
+
+cursos.get('/:id', obtenerCurso, function(req, res) {
+  res.locals.id = req.params.id;
+  res.render('curso/ver');
+});
+
+cursos.get('/:id/detalles', obtenerCurso, function(req, res) {
+  res.locals.id = req.params.id;
+  res.render('curso/detalles');
+});
+
+app.use('/cursos', cursos);
+
+app.listen(8000, function () {
+  console.log("Esperando requests en el puerto 8000");
 });
